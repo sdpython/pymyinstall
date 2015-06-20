b7ab8601b7c73cf0364b5454c79fd798051621ea
xavier dupré <xavier.dupre@gmail.com>
2015-06-20 22:06:57 +0000

    format

--- build_win_setup.py
+++ build_win_setup.py
@@ -16,7 +16,6 @@ if False:
 
 
 if True:
-    win_python_setup(module_list=list_modules, verbose=True, 
+    win_python_setup(module_list=list_modules, verbose=True,
                      download_only=False,
-                     no_setup=True,
-                     step_skip=None) #["julia_install", "julia_build", "r_install"])
+                     no_setup=True)
