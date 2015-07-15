43f2c961c0ec687a8432e5f586d430d9b29726d4
xavier dupré <xavier.dupre@gmail.com>
2015-07-15 23:25:18 +0000

    update script import_all.py

--- import_all.py
+++ import_all.py
@@ -5,6 +5,9 @@ print(platform.architecture())
 sys.path.append("src")
 from pymyinstall import installation_ensae
 skip = ["pyquickhelper", "pyensae", "pyrsslocal",
-        "pymmails", "ensae_teaching_cs"]
+        "pymmails", "ensae_teaching_cs", "code_beatrix", 
+        "actuariat_python"]
 for mod in installation_ensae():
+    if mod.name in skip:
+        continue
     mod.install(log=True)
