b1c9259e030815d185541845d94c191a8a762a73
dupre <xavier.dupre@ensae.fr>
2015-09-02 00:12:17 +0200

    fix import issue

--- src/pymyinstall/packaged/packaged_config.py
+++ src/pymyinstall/packaged/packaged_config.py
@@ -9,4 +9,4 @@ from .packaged_config_ml import ml_set, ensae_set
 from .packaged_config_small import small_set
 from .packaged_config_sphinx import sphinx_theme_set
 from .packaged_config_teachings import teachings_set
-from .packaged_iot import iot_set
+from .packaged_config_iot import iot_set
