555407feba499154784bcb9b792b9d6ee54feb2a
dupre <xavier.dupre@ensae.fr>
2015-09-11 22:28:36 +0200

    update version number

--- src/pymyinstall/installhelper/module_install_version.py
+++ src/pymyinstall/installhelper/module_install_version.py
@@ -544,12 +544,12 @@ def get_module_dependencies(module, use_cmd=False, deep=False, collapse=True, us
         if module not in _get_module_dependencies_deps:
             raise ValueError("module {0} was not installed".format(module))
         res = []
-        for req in _get_module_dependencies_deps[module]:
-            if isinstance(req, list):
-                for r in req:
-                    res.append((r.key, None, module))
-            else:
-                res.append((req.key, None, module))
+        dist, req = _get_module_dependencies_deps[module] 
+        if isinstance(req, list):
+            for r in req:
+                res.append((r.key, None, module))
+        else:
+            res.append((req.key, None, module))
     else:
         meta = get_module_metadata(module, use_cmd)
         deps = [v for k, v in meta.items() if "Requires" in k]
