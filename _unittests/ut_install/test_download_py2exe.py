8c3eba3e26aa31e2d03f7ec6d34273532a659b80
dupre <xavier.dupre@ensae.fr>
2015-11-08 14:28:38 +0100

    update regular expression for py2exe

---
+++ _unittests/ut_install/test_download_py2exe.py
@@ -0,0 +1,62 @@
+"""
+@brief      test log(time=20s)
+"""
+
+import sys
+import os
+import unittest
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+try:
+    import pyquickhelper
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    if "PYQUICKHELPER" in os.environ and len(os.environ["PYQUICKHELPER"]) > 0:
+        sys.path.append(os.environ["PYQUICKHELPER"])
+    import pyquickhelper
+
+
+from src.pymyinstall.packaged import find_module_install
+from pyquickhelper import fLOG, get_temp_folder
+
+
+class TestDownloadPy2exe(unittest.TestCase):
+
+    def test_install_py2exe(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+        temp = get_temp_folder(__file__, "temp_download_py2exe")
+
+        if sys.platform.startswith("win"):
+            m = find_module_install("py2exe")
+            m.fLOG = fLOG
+            whl = m.download(temp_folder=temp)
+            assert os.path.exists(whl)
+
+
+if __name__ == "__main__":
+    unittest.main()
