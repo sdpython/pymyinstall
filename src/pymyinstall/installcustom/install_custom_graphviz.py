c99f4ebd866f275f94da1ba02b09a97e5b8829b0
dupre <xavier.dupre@ensae.fr>
2016-05-30 22:08:55 +0200

    add logging information

--- src/pymyinstall/installcustom/install_custom_graphviz.py
+++ src/pymyinstall/installcustom/install_custom_graphviz.py
@@ -57,6 +57,7 @@ def install_graphviz(temp_folder=".", fLOG=print, install=True,
         local = download_file(url, outfile)
         if install:
             if source == "zip":
+                fLOG("dest ", os.path.abspath(temp_folder))
                 unzip_files(local, temp_folder, fLOG=fLOG)
             else:
                 run_cmd("msiexec /i " + local, fLOG=fLOG, wait=True)
