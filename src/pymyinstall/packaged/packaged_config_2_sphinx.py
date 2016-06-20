a091ce197a748ccd5e534dbf216dc94726bf0d4f
dupre <xavier.dupre@ensae.fr>
2016-06-21 00:59:57 +0200

    add latex extension

--- src/pymyinstall/packaged/packaged_config_2_sphinx.py
+++ src/pymyinstall/packaged/packaged_config_2_sphinx.py
@@ -73,5 +73,7 @@ def sphinx_theme_set():
                       purpose="sphinx theme", usage="SPHINX"),
         ModuleInstall("sphinx_theme_pd", "pip", mname="sphinx_theme_pd",
                       purpose="sphinx theme", usage="SPHINX"),
+        ModuleInstall("sphinx_latex", "pip", mname="sphinx_latex",
+                      purpose="latex extension", usage="SPHINX"),
     ]
     return [_ for _ in mod if _ is not None]
