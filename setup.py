1ca166acf24c48cbd3bc961da108f92b6011a0f0
dupre <xavier.dupre@ensae.fr>
2016-07-26 16:27:50 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ sversion = "1.1"
 versionPython = "%s.%s" % (sys.version_info.major, sys.version_info.minor)
 path = "Lib/site-packages/" + project_var_name
 readme = 'README.rst'
-
+requirements = None
 
 KEYWORDS = project_var_name + ', installation, Xavier Dupré'
 DESCRIPTION = """Easy installation of modules for data scientists"""
@@ -27,9 +27,11 @@ CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable'
 ]
 
+
 #######
 # data
 #######
+
 
 packages = find_packages('src', exclude='src')
 package_dir = {k: "src/" + k.replace(".", "/") for k in packages}
@@ -67,6 +69,7 @@ def is_local():
        "unittests" in sys.argv or \
        "unittests_LONG" in sys.argv or \
        "unittests_SKIP" in sys.argv or \
+       "unittests_GUI" in sys.argv or \
        "run27" in sys.argv or \
        "sdist" in sys.argv or \
        "setupdep" in sys.argv or \
@@ -121,7 +124,7 @@ def verbose():
 # version
 ##########
 
-if is_local():
+if is_local() and "--help" not in sys.argv and "--help-commands" not in sys.argv:
     def write_version():
         pyquickhelper = import_pyquickhelper()
         from pyquickhelper.pycode import write_version_for_setup
@@ -134,6 +137,8 @@ if is_local():
         with open("version.txt", "r") as f:
             lines = f.readlines()
         subversion = "." + lines[0].strip("\r\n ")
+        if subversion == ".0":
+            raise Exception("subversion is wrong: " + subversion)
     else:
         raise FileNotFoundError("version.txt")
 else:
@@ -157,9 +162,9 @@ if "--verbose" in sys.argv:
 
 if is_local():
     pyquickhelper = import_pyquickhelper()
-    from pyquickhelper.loghelper import fLOG as logging_function
-    logging_function(OutputPrint=True)
+    logging_function = pyquickhelper.get_fLOG()
     from pyquickhelper.pycode import process_standard_options_for_setup
+    logging_function(OutputPrint=True)
     r = process_standard_options_for_setup(
         sys.argv, __file__, project_var_name,
         # not need for the regular setup, just for the documentation, unit
@@ -184,12 +189,11 @@ if is_local():
 else:
     r = False
 
-if len(sys.argv) == 1 and "--help" in sys.argv:
-    pyquickhelper = import_pyquickhelper()
-    from pyquickhelper.pycode import process_standard_options_for_setup_help
-    process_standard_options_for_setup_help()
-
 if not r:
+    if len(sys.argv) in (1, 2) and sys.argv[-1] in ("--help-commands",):
+        pyquickhelper = import_pyquickhelper()
+        from pyquickhelper.pycode import process_standard_options_for_setup_help
+        process_standard_options_for_setup_help(sys.argv)
 
     if sys.version_info[0] >= 3:
         entry_points = {
