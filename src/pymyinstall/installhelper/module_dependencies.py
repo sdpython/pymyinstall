9b5ac1eb0c60ee55875501df68a92737b2b8f994
xavier dupré <xavier.dupre@gmail.com>
2017-03-19 16:42:51 +0100

    fix syntax issue

--- src/pymyinstall/installhelper/module_dependencies.py
+++ src/pymyinstall/installhelper/module_dependencies.py
@@ -27,7 +27,7 @@ def missing_dependencies(specific=None):
     the function relis on module pipdeptree.
     """
     skip = {"setuptools", "pip", "distribute", "ordereddict", "argparse", "futures",
-            "pywin32", "pywin32_ctypes", "pypiwin32", "monotonic", "typing", "pathlib",,
+            "pywin32", "pywin32_ctypes", "pypiwin32", "monotonic", "typing", "pathlib",
             "enum_compat"}
     tree = _main_pipdeptree()
     stack = {}
