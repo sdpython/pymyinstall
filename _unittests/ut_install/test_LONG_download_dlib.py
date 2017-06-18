ad638ef488c8040771b824ec2b26ac2727b2b7bd
xavier dupré <xavier.dupre@gmail.com>
2017-06-18 10:22:20 +0200

    add unit test on dlib

---
+++ _unittests/ut_install/test_LONG_download_dlib.py
@@ -0,0 +1,65 @@
+"""
+@brief      test log(time=14s)
+"""
+
+import sys
+import os
+import unittest
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+try:
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    if "PYQUICKHELPER" in os.environ and len(os.environ["PYQUICKHELPER"]) > 0:
+        sys.path.append(os.environ["PYQUICKHELPER"])
+    import pyquickhelper as skip_
+
+
+from src.pymyinstall.packaged import find_module_install
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import get_temp_folder
+
+
+class TestLONGDownloaddlib(unittest.TestCase):
+
+    def test_install_dlib(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+        temp = get_temp_folder(__file__, "temp_download_dlib")
+
+        if sys.platform.startswith("win"):
+            m = find_module_install("dlib", must_exist=True)
+            fLOG(m)
+            fLOG(m.__module__)
+            m.fLOG = fLOG
+            whl = m.download(temp_folder=temp, source="2")
+            self.assertTrue(os.path.exists(whl))
+
+
+if __name__ == "__main__":
+    unittest.main()
