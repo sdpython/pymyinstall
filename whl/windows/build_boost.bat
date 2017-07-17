7f275549bd8735c83ed6c05166a613abc50f624a
xavier dupré <xavier.dupre@gmail.com>
2017-07-17 20:07:55 +0200

    building boost on windows is like a mystery

--- whl/windows/build_boost.bat
+++ whl/windows/build_boost.bat
@@ -1,4 +1,5 @@
 @echo off
+@echo [boost] begin
 if "%1"=="" goto default_value_python:
 if "%1"=="default" goto default_value_python:
 set pythonexe=%1
@@ -9,6 +10,14 @@ set pythonexe=c:\Python36_x64
 
 :start_script:
 set current=%~dp0
+
+@echo [boost] vcvarsall.bat
+pushd "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
+call vcvarsall.bat x64
+popd
+@echo [boost] vcvarsall.bat Done
+
+@echo [boost] mkdir
 set PATH=%pythonexe%;%pythonexe%\Scripts;%PATH%
 if not exist boost mkdir boost
 pushd boost
@@ -17,27 +26,35 @@ pushd boost
 set version=1_64_0
 set version2=1.64.0
 if exist %current%boost\boost_%version% goto booststrap:
-@echo Download boost
+@echo [boost] Download
 bitsadmin /transfer wcb /priority high https://dl.bintray.com/boostorg/release/%version2%/source/boost_%version%.7z %current%boost\boost_%version%.7z
 set PATH=%PATH%;C:\Program Files\7-Zip\
 @echo Unzip xgboost
 7z x %current%boost\boost_%version%.7z -o%current%boost\boost_%version%
 
 :booststrap:
-@echo BOOTSTRAP ---------------------
+@echo [boost] BOOTSTRAP ---------------------
 cd %current%boost\boost_%version%\boost_%version%\tools\build
-rem cmd /c bootstrap
+cmd /c bootstrap
 
 :b2:
-@echo B2 ---------------------
-rem cmd /c b2 install --prefix=%current%boost\MYINST
+@echo [boost] B2 ---------------------
+cmd /c b2 install --prefix=%current%boost\MYINST
 
 :python-config:
-@echo [config]
-rem @echo "using python : 3.6;" >> boost\MYINST\share\boost-build\example\user-config.jam
+@echo [boost] config in %current%boost\MYINST\share\boost-build\example\user-config.jam
+if not exist %current%boost\MYINST\share\boost-build\example\user-config.jam goto erroruser:
+@echo "using python : 3.6: c:/Python36_x64/python.exe : c:/Python36_x64/include : c:/Python36_x64/libs ;" >> %current%boost\MYINST\share\boost-build\example\user-config.jam
+goto build:
+
+:erroruser:
+@echo Cannot find %current%boost\MYINST\share\boost-build\example\user-config.jam.
+exit 1
+
 
 :build:
-@echo [build]
+@echo [boost] build
 set PATH=%PATH%;%current%boost\MYINST\bin
 cd %current%boost\boost_%version%\boost_%version%
-b2 --build-dir=%current%boost\build toolset=msvc --with-python --build-type=complete architecture=x86 address-model=64 runtime-link=static msvc stage
+b2 --build-dir=%current%boost\build toolset=msvc --stagedir=stage/x64 --without-python --build-type=complete architecture=x86 address-model=64 runtime-link=static msvc stage
+@echo [boost] done
