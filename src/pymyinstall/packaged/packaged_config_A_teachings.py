dec9b9322e388cb122a48e4b3decba2d63c53ff5
xavier dupré <xavier.dupre@gmail.com>
2017-12-29 01:24:49 +0100

    add teachings modules

--- src/pymyinstall/packaged/packaged_config_A_teachings.py
+++ src/pymyinstall/packaged/packaged_config_A_teachings.py
@@ -39,6 +39,18 @@ def teachings_set():
                       purpose="materials for teachings", usage="TEACH"),
         ModuleInstall("ensae_projects", "pip",
                       purpose="single use code", usage="TEACH"),
+        ModuleInstall("mlinsights", "pip",
+                      purpose="mlinsights implements functions to get insights on machine learned models.", usage="TEACH"),
+        ModuleInstall("lightmlrestapi", "pip",
+                      purpose="lightmlrestapi implements a light machine learning REST API based on falcon.", usage="TEACH"),
+        ModuleInstall("lightmlboard", "pip",
+                      purpose="lightmlboard implements a light machine learning leaderboard based on tornado.", usage="TEACH"),
+        ModuleInstall("mlprodicts", "pip",
+                      purpose="mlprodict implements a couple of ways to productionize machine learning predictions.", usage="TEACH"),
+        ModuleInstall("pandas_streaming", "pip",
+                      purpose="pandas_streaming aims at processing big files with pandas, too big to hold in memory," +
+                      "too small to be parallelized with a significant gain. The module replicates a subset of pandas " +
+                      "API and implements other functionalities for machine learning.", usage="TEACH"),
     ]
     #
     return [_ for _ in mod if _ is not None]
