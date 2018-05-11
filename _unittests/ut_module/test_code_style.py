1c77d30159c54bb056d3f6ee6be5bd2a07d3d2a0
xavier dupré <xavier.dupre@gmail.com>
2018-05-12 00:10:30 +0200

    requirements, disable one test for pylint

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -65,7 +65,7 @@ class TestCodeStyle(ExtTestCase):
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'W0703', 'W0622', 'W0621', 'C0412'),
+                                  'C0111', 'W0703', 'W0622', 'W0621', 'C0412', 'C0411'),
                    skip=["src' imported but unused",
                          "skip_' imported but unused",
                          "skip__' imported but unused",
