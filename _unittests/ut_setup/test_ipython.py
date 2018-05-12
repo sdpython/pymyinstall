0f0cfd931071858d839bd73b36b5b52c951ddcc2
xavier dupré <xavier.dupre@gmail.com>
2018-05-12 09:34:19 +0200

    disable a test on linux

--- _unittests/ut_setup/test_ipython.py
+++ _unittests/ut_setup/test_ipython.py
@@ -5,6 +5,8 @@
 import sys
 import os
 import unittest
+from pyquickhelper.loghelper import fLOG
+
 
 try:
     import src
@@ -19,31 +21,13 @@ except ImportError:
         sys.path.append(path)
     import src
 
-try:
-    import pyquickhelper as skip_
-except ImportError:
-    path = os.path.normpath(
-        os.path.abspath(
-            os.path.join(
-                os.path.split(__file__)[0],
-                "..",
-                "..",
-                "..",
-                "pyquickhelper",
-                "src")))
-    if path not in sys.path:
-        sys.path.append(path)
-    if "PYQUICKHELPER" in os.environ and len(os.environ["PYQUICKHELPER"]) > 0:
-        sys.path.append(os.environ["PYQUICKHELPER"])
-    import pyquickhelper as skip_
-
 
 from src.pymyinstall.setuphelper.ipython_helper import setup_ipython
-from pyquickhelper.loghelper import fLOG
 
 
 class TestSetupIPython (unittest.TestCase):
 
+    @unittest.skipIf(not sys.platform.startswith("win"), "not implemented on Windows")
     def test_setup(self):
         fLOG(
             __file__,
