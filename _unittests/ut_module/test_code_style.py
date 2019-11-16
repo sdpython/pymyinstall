f04ccbddbdc546c1c704d541f3c19837f85c79c1
xavier dupré <xavier.dupre@gmail.com>
2019-11-16 11:07:14 +0100

    Update test_code_style.py

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -48,6 +48,7 @@ class TestCodeStyle(ExtTestCase):
                          "Unable to import 'Queue'",
                          "Unable to import 'src.pymyinstall.installhelper.status_helper'",
                          "[W503] line break before binary operator",
+                         "install_custom_python.py:163",
                          ])
 
     def test_style_test(self):
