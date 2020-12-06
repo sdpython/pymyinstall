2209e57589ed123ff7693ac044ddca77ef24f666
xavier dupré <xavier.dupre@gmail.com>
2020-12-06 18:32:57 +0100

    Update test_download_gmpy_kivy.py

--- _unittests/ut_install/test_download_gmpy_kivy.py
+++ _unittests/ut_install/test_download_gmpy_kivy.py
@@ -24,18 +24,6 @@ class TestDownload2 (unittest.TestCase):
             whl = m.download(temp_folder=temp, source="2")
             self.assertTrue(os.path.exists(whl))
 
-    def test_install_kivy(self):
-        fLOG(
-            __file__,
-            self._testMethodName,
-            OutputPrint=__name__ == "__main__")
-        temp = get_temp_folder(__file__, "temp_download_kivy")
-
-        if sys.platform.startswith("win"):
-            m = ModuleInstall("Kivy", "wheel", mname="kivy", fLOG=fLOG)
-            whl = m.download(temp_folder=temp, source="2")
-            self.assertTrue(os.path.exists(whl))
-
 
 if __name__ == "__main__":
     unittest.main()
